757d58c2f812562daa344e15588a063a559d7936
judai39 <dhy2522609443@163.com>
2024-12-02 13:53:02 +0800

    Kotlin Design Patterns Create Function Singleton

--- Kotlin/Kotlin/src/zzz_design_patterns/create/singleton/SingletonTest.kt
+++ Kotlin/Kotlin/src/zzz_design_patterns/create/singleton/SingletonTest.kt
@@ -2,7 +2,6 @@ package zzz_design_patterns.create.singleton
 
 import javax.naming.Context
 
-//以Volley的单例创建举例
 class SingletonTest private constructor(context: Context){
     companion object{
         private var INSTANCE:SingletonTest?=null
