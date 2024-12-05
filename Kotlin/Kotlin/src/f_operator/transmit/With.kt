4e7906bd4361978b3c97e78ac2c66de5b4f76c86
judai39 <dhy2522609443@163.com>
2024-12-05 09:42:31 +0800

    How To With

---
+++ Kotlin/Kotlin/src/f_operator/transmit/With.kt
@@ -0,0 +1,17 @@
+package f_operator.transmit
+
+/**在调用一个实例对象的成员时,可以使用also包围,在代码块中直接使用(无需调用)*/
+class StudentWith{
+    fun say()=print("hello")
+    fun eat()= print("have a lunch")
+    fun sleep()= print("have a sleep")
+}
+
+fun main() {
+    var student=StudentWith()
+    with(student){
+        say()
+        eat()
+        sleep()
+    }
+}
