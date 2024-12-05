5bada15c141b5b8c8f134ecf749fe182f6f9eae9
judai39 <dhy2522609443@163.com>
2024-12-05 09:47:11 +0800

    How To Run

---
+++ Kotlin/Kotlin/src/f_operator/transmit/Run.kt
@@ -0,0 +1,22 @@
+package f_operator.transmit
+
+/**run结合了let,with两种功能
+ * 1,surround后无需调用直接使用成员
+ * 2,统一判空
+ * 3,定义一个变量在特定作用域
+ * */
+
+class StudentRun{
+    fun say()=print("hello")
+    fun eat()= print("have a lunch")
+    fun sleep()= print("have a sleep")
+}
+
+fun main() {
+    var student=StudentRun()
+    student?.run {
+        say()
+        eat()
+        sleep()
+    }
+}
