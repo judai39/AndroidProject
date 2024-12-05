ba3e697c53eb1afa6485bb1fe5e22b08dcd4c4dc
judai39 <dhy2522609443@163.com>
2024-12-05 09:34:10 +0800

    How To Let

---
+++ Kotlin/Kotlin/src/f_operator/transmit/Let.kt
@@ -0,0 +1,25 @@
+package f_operator.transmit
+
+
+class Student{
+    fun say()=print("hello")
+    fun eat()= print("have a lunch")
+    fun sleep()= print("have a sleep")
+}
+
+fun main() {
+    //作用1:使用it替代object对象去访问其公有的属性和方法
+    val student=Student()
+    student.let {
+        it.say()
+        it.eat()
+        it.sleep()
+    }
+    //作用2:自动判断null传递
+    student?.let {
+        it.say()
+        it.eat()
+        it.sleep()
+    }//如果student对象不为空,则调用
+    /**返回值为最后的表达式return*/
+}
